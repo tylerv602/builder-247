c2549d7a5c5e473c3201d13336f5a0cf4d178236
tylerv602 <tylerv602@users.noreply.github.com>
2025-05-06 21:09:20 +0000

    Update jest configuration to support TypeScript tests

--- middle-server/jest.config.js
+++ middle-server/jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"],
-  moduleFileExtensions: ["ts", "js"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.ts',
+    '**/?(*.)+(spec|test).ts'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  }
 };
